ed2e7766a298f5e7f888a527cb0dc21812c61730
Ben Baker <bakerb845@gmail.com>
2021-05-25 14:04:07 -0600

    improving cmake for newer versions of cmake

--- CMakeModules/FindMiniSEEDConfig.cmake
+++ CMakeModules/FindMiniSEEDConfig.cmake
@@ -3,11 +3,7 @@ if (MINISEED_INCLUDE_DIR AND MINISEED_LIBRARY)
     set(MINISEED_FIND_QUIETLY TRUE)
 endif()
 
-#if (NOT BUILD_SHARED_LIBS)
-    #set(MINISEED "libmseed.a")
-#else()
-    set(MINISEED mseed)
-#endif
+set(MINISEED mseed)
 
 # Find the include directory
 find_path(MINISEED_INCLUDE_DIR
@@ -23,9 +19,8 @@ find_library(MINISEED_LIBRARY
                    /usr/local/lib64
                    /usr/local/lib
             )
-# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
-# all listed variables are TRUE.
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MINISEED
+find_package_handle_standard_args(FindMiniSEED
                                   DEFAULT_MSG MINISEED_INCLUDE_DIR MINISEED_LIBRARY)
 mark_as_advanced(MINISEED_INCLUDE_DIR MINISEED_LIBRARY)
